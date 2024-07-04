d71bfb35ba7588626091ff53b06d1ce59ad411ad
Deyao Chen <chendeyao000@gmail.com>
2024-07-04 22:15:01 +0000

    improve run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
-make -C build CFLAGS="-DDEBUG_SLOW=1 -Wall -g -O3"
+make -C build CFLAGS="-UDEBUG_SLOW -Wall -O3"
 
-# build/gen_points 9 build/large_points.txt 50000
-build/l2_subset build/large_points.txt 1000
-cd ..
+build/gen_points 3 build/large_points.txt 10000
+build/l2_subset build/large_points.txt 10 | tee build/l2_subset_log.txt
+cat build/l2_subset_log.txt | grep "Active points:" | sed 's/Active points: //g' > build/selected_points.txt
+echo "Verifying descrepancy of selected points with python script"
+python3 formula.py build/large_points.txt build/selected_points.txt
+
