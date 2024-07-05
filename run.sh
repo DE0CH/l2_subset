a295a9796cba6a50c8778dbdc8b35776427db417
Deyao Chen <chendeyao000@gmail.com>
2024-07-05 20:10:13 +0100

    fix run script

--- run.sh
+++ run.sh
@@ -3,26 +3,42 @@
 set -eo pipefail
 
 make -C build clean
-make -C build CFLAGS="-UDEBUG_SLOW -DCOMPUTE_MODE=2 -Wall -O3"
-N=100000
-M=50
-DIM=3
-TRIALS=100
+make -C build CFLAGS="-UDEBUG_SLOW -DCOMPUTE_MODE=2 -Wall -O3 -g"
+export N=500000
+export M=50
+export DIM=3
+TRIALS=300
 echo "Generating $N points"
 build/gen_points "$DIM" build/large_points.txt "$N"
 
 echo "Compiling matrix"
 build/l2_subset_compile_matrix build/large_points.txt "$M" build/large_points.p
-for i in $(seq 1 "$TRIALS"); do
-    TEMP=`mktemp`
-    echo "Starting trial $i"
-    build/l2_subset_from_compiled_matrix build/large_points.p $i | tee build/l2_subset_log.txt
-    cat build/l2_subset_log.txt | grep "Active points:" | sed 's/Active points: //g' > build/selected_points.txt
-    echo "Verifying trial $i"
-    python3 src/formula.py build/large_points.txt build/selected_points.txt
-    python3 src/filter_point.py build/large_points.txt build/selected_points.txt > build/selected_pf.txt
-    build/linf_subset build/selected_pf.txt "$DIM" "$M" "$M"  > $TEMP 2>&1 || true # There is some segfault at the end of the program, and we don't really care. 
-    echo -n "L infinity: "
-    grep -oP "Final discre:\K[0-9.]+" $TEMP 
-    rm $TEMP
-done
+echo "finished compiling matrix"
+# Function to execute a single trial
+run_trial() {
+    set -eo pipefail  # Ensure error handling is applied within the function
+    
+    local trial=$1
+    local temp_dir=$(mktemp -d)
+    local output_file=$(mktemp)
+    $(
+    {
+        echo "Starting trial $trial"
+        build/l2_subset_from_compiled_matrix build/large_points.p "$trial" | tee "$temp_dir/l2_subset_log.txt"
+        grep "Active points:" "$temp_dir/l2_subset_log.txt" | sed 's/Active points: //g' > "$temp_dir/selected_points.txt"
+        echo "Verifying trial $trial"
+        python3 src/formula.py build/large_points.txt "$temp_dir/selected_points.txt"
+        python3 src/filter_point.py build/large_points.txt "$temp_dir/selected_points.txt" > "$temp_dir/selected_pf.txt"
+        build/linf_subset "$temp_dir/selected_pf.txt" "$DIM" "$M" "$M" > "$temp_dir/output.txt" 2>&1 || true # There is some segfault at the end of the program, and we don't really care.
+        echo -n "Trial $trial: L infinity: "
+        grep -oP "Final discre:\K[0-9.]+" "$temp_dir/output.txt" | head -1
+        rm -r "$temp_dir"
+    } &> "$output_file"
+    )
+    cat "$output_file"
+    rm "$output_file"
+}
+export -f run_trial
+
+# Run the trials in parallel
+parallel -j $(($(nproc) - 1))  run_trial ::: $(seq 1 "$TRIALS")
