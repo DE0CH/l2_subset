53aa73fa2a192d47317fe51a3f52485af5007f41
Deyao Chen <chendeyao000@gmail.com>
2024-07-09 13:14:13 +0100

    update run script to not depend on numpy

--- src/filter_point.py
+++ src/filter_point.py
@@ -1,19 +1,20 @@
 import argparse
-import numpy as np
 
 parser = argparse.ArgumentParser(description='Select points by index')
 parser.add_argument('point_file', type=str, help='Point File')
-parser.add_argument('selected_points', type=str, help='Selected Points File') 
+parser.add_argument('selected_points', type=str, help='Selected Points File')
 args = parser.parse_args()
 
 with open(args.selected_points) as f:
-    selected_points = np.array(list(map(int, f.readline().split())))
+    selected_points = list(map(int, f.readline().split()))
 
 with open(args.point_file) as f:
-    f.readline()
+    d, n, _ = f.readline().split()
     points = []
     for line in f:
         points.append(line)
-    points = np.array(points)
-    print(''.join(points[selected_points]), end='')
-
+    remaining_points = []
+    for i in selected_points:
+        remaining_points.append(points[i])
+print(d, len(selected_points), "0.0", sep=' ')
+print(''.join(remaining_points), end='')
