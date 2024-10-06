0a941cd868bc0f627f610d56e7e7da51c75c7262
Deyao Chen <chendeyao000@gmail.com>
2024-10-06 17:40:05 +0000

    add new mode draft

---
+++ src/l2_subset_from_compiled_matrix_w_starting_point.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <sys/mman.h>
+#include "l2_subset.h"
+
+int main(int argc, char *argv[]) {
+    struct input_data data;
+    void *mmaped_data;
+    struct weights *w = read_from_compiled_matrix(&data, argc, argv, &mmaped_data);
+    init_genrand64(data.seed);
+    struct analytics *a = main_loop(w);
+    print_results(w, a);
+    analytics_free(a);
+    free_mmaped_matrix(w, mmaped_data);
+    w->points = malloc(0);
+#if COMPUTE_MODE == USE_MATRIX
+    w->entries = malloc(0);
+#elif COMPUTE_MODE == USE_POINTS
+#else
+    #error "Invalid COMPUTE_MODE"
+#endif
+    weights_free(w);
+    return 0;
+}
