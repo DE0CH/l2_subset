cd7934686e043fe9791e59160c8ce1ecefd51f21
Deyao Chen <chendeyao000@gmail.com>
2024-10-06 21:55:04 +0000

    add perturbation python script

---
+++ src/perturbe.py
@@ -0,0 +1,71 @@
+import struct
+import argparse
+import subprocess
+import random
+import re
+import sys
+
+def read_numbers_from_file(filename):
+    with open(filename, "rb") as file:  # Open the file in binary mode
+        # Read the first 24 bytes (3 long long values, 8 bytes each)
+        data = file.read(24)
+        
+        if len(data) < 24:
+            raise ValueError("File is too small to contain 3 long long numbers.")
+        
+        # Unpack the data as three long long integers ('q' format specifier)
+        n, m, d = struct.unpack('qqq', data)
+        
+        return n, m, d
+
+parser = argparse.ArgumentParser()
+parser.add_argument('point_file', type=argparse.FileType('rb'), help='Point file')
+parser.add_argument('seed', type=int, help='Seed for random number generator')
+parser.add_argument('perturbation_number', type=int, help="number of points to pertube")
+parser.add_argument('iterations', type=int, help='Number of iterations')
+args = parser.parse_args()
+
+random.seed(args.seed)
+
+best_l2 = 1
+best_linf = 1
+n, m, d = read_numbers_from_file(args.point_file.name)
+points = random.sample(list(range(n)), m)
+
+for i in range(args.iterations):
+    lines = []
+    p = subprocess.Popen(["./l2_subset_from_compiled_matrix_w_starting_point", args.point_file.name, str(random.randrange(0, 2**61-1)), *map(str, points)], stdout=subprocess.PIPE)
+    for line in p.stdout:
+        line = line.decode('utf-8')
+        print(line, end='')
+        matches = re.match(r"Active points: ([\d\s]+)", line)
+        if matches:
+            new_points = list(map(int, matches.group(1).split()))
+        matches = re.match(r"Active point sum: ([\d\.]+)", line)
+        if matches:
+            new_l2 = float(matches.group(1))
+        matches = re.match(r"linf discrepancy: ([\d\.]+)", line)
+        if matches:
+            new_linf = float(matches.group(1))
+        lines.append(line)
+    p.wait()
+    # check return code
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, p.args)
+
+    if new_l2 < best_l2:
+        best_l2 = new_l2
+        best_linf = new_linf
+        points = new_points
+    other_points = []
+    points_set = set(points)
+    for i in range(n):
+        if i not in points_set:
+            other_points.append(i)
+    for i in range(args.perturbation_number):
+        old = random.randrange(0, m)
+        new = random.randrange(0, n-m)
+        t = points[old]
+        points[old] = other_points[new]
+        other_points[new] = t
+    points = new_points
