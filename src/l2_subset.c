6a8e2876b7e3059cdbe7ca47fe2e39cf5efe8de6
Deyao Chen <chendeyao000@gmail.com>
2024-10-23 17:28:02 +0000

    swap formula from own formula to francois formula 1

--- src/l2_subset.c
+++ src/l2_subset.c
@@ -181,18 +181,21 @@ void calculate_init_weights(struct weights *w) {
 }
 
 double w_ij(double* X, size_t i, size_t j, long long d, long long m, long long n) {
+    // n is the total number of points in the set and m is the number of points in the subset
+    // we only care about m
+    double alpha = 0.1;
     if (i == j) {
         double prod1 = 1.0;
         double prod2 = 1.0;
         for (size_t h = 0; h < d; h++) {
-            prod1 *= (3 - pow(X[i * d + h], 2));
-            prod2 *= (2 - X[i * d + h]);
+            prod1 *= (1 - pow(X[i * d + h], alpha +  1)) / (alpha + 1);
+            prod2 *= (1 - pow(X[i * d + h], alpha +  2)) / (alpha + 2);
         }
-        return - prod1 * pow(2, 1-d) / m + prod2 / (m * m);
+        return prod2 / (m * m) - prod1 * 2 / m;
     } else {
         double prod = 1.0;
         for (size_t h = 0; h < d; h++) {
-            prod *= min(2 - X[i * d + h], 2 - X[j * d + h]);
+            prod *= (1 - pow(max(X[i * d + h], X[j * d + h]), alpha + 1)) / (alpha + 1);
         }
         return prod / (m * m);
     }
